1576880c6a7043fba6107c70d91959c1f26c8a2e
Yuriy Khomyakov <y.khomyakov@rambler-co.ru>
2012-08-15 14:02:29 +0400

    bump version

--- src/setup.py
+++ src/setup.py
@@ -3,16 +3,17 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.md')).read()
+README = open(os.path.join(here, 'README.rst')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires=['pyramid>=1.3', 'pyramid_jinja2', 'wtforms', 'webhelpers']
 
 setup(name='pyramid_admin',
-      version='0.3',
+      version='0.4',
       description='admin (CRUD) interface for pyramid framework',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
+        "Development Status :: 4 - Beta",
         "Programming Language :: Python",
         "Framework :: Pyramid",
         "Topic :: Internet :: WWW/HTTP",
